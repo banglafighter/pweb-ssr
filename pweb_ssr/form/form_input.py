8e113c6fea5d9e1bffe8b7bd471a1f8089c8843f
hmtmcse <hmtm.cse@gmail.com>
2023-09-29 11:47:47 +0600

    Working on SSR

--- pweb_ssr/form/form_input.py
+++ pweb_ssr/form/form_input.py
@@ -1,8 +1,9 @@
+from pweb_form_rest import FormField
 from pweb_ssr.lib.jinja_tag_helper import StandaloneTag
 
 
 class FormInput(StandaloneTag):
     tags = {"form_input"}
 
-    def render(self, *args, **kwargs):
+    def render(self, field: FormField, *args, **kwargs):
         return "Working Fine"
