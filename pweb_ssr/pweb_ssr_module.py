71c5fbaf7ef47097204b13ea8037cf6403648294
hmtmcse <hmtm.cse@gmail.com>
2023-10-04 13:30:11 +0600

    refactor code

--- pweb_ssr/pweb_ssr_module.py
+++ pweb_ssr/pweb_ssr_module.py
@@ -1,9 +1,12 @@
-from pweb import PWebComponentRegister
+from pweb import PWebComponentRegister, PWebModuleDetails
 from pweb_ssr.common.pweb_ssr_init import PWebSSRInit
 from pweb_ssr.pweb_jinja_extend import PWebJinjaExtend
 
 
 class PWebSSRModule(PWebComponentRegister):
+
+    def app_details(self) -> PWebModuleDetails:
+        return PWebModuleDetails(system_name="pweb-ssr", display_name="PWeb SSR Module")
 
     def run_on_cli_init(self, pweb_app, config):
         PWebSSRInit.merge_config(config=config)
